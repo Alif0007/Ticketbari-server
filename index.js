432d2cf2a886b1e3a35a8d2b27d6d575daab2992
Al Amin Alif <alaminalif373@gmail.com>
2025-12-22 22:00:18 +0600

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
 
 
@@ -339,7 +339,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Worldsdaaaaaaaaaaaaa22333!')
+    res.send('Hello Worldsdaaaaaaaaaaaaa22333test!')
 })
 
 app.listen(port, () => {
